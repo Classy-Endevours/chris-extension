29ca60caa0865f077c7b0da0efef90e26013ca3f
Manthan Mehta <manthan81096@gmail.com>
2023-01-11 04:27:57 +0530

    Combine both extension

--- text-selection/src/inject/index.tsx
+++ text-selection/src/inject/index.tsx
@@ -3,15 +3,56 @@ import * as ReactDOM from "react-dom";
 
 function Inject() {
   const [text, setText] = useState<string>("");
+  const [buttonCss, setButtonCss] = useState({
+    left: "0",
+    top: "0",
+  });
+  const [textArea, setTextArea] = useState<string>("");
+  const handleText = (e) => {
+    if (e.key == "Enter") {
+      // @ts-ignore
+      setTextArea(document.activeElement.value + "\n");
+    }
+  };
+  const handleFocusIn = (e) => {
+    // @ts-ignore
+    const tempMeasure = e.path[0].getBoundingClientRect();
+    console.log({ tempMeasure: e.path[0] });
+    setButtonCss({
+      top: tempMeasure.top + e.path[0].offsetTop,
+      left: tempMeasure.left + e.path[0].offsetWidth,
+    });
+
+    if (
+      document.activeElement.nodeName == "TEXTAREA" ||
+      document.activeElement.nodeName == "INPUT"
+    ) {
+      document.addEventListener("keypress", handleText);
+    }
+  };
+  const handleFocusOut = () => {
+    setTextArea("");
+    document.removeEventListener("keypress", handleText);
+  };
   useEffect(() => {
-    document.onmouseup = () => {
-      const textTemp = getSelectedText();
-      if (textTemp != "") setText(textTemp);
-    };
-    document.onmousedown = () => {
-      setText("");
+    document.addEventListener("focusin", handleFocusIn);
+    document.addEventListener("focusout", handleFocusOut);
+    return () => {
+      document.removeEventListener("focusin", handleFocusIn);
+      document.removeEventListener("focusout", handleFocusOut);
     };
   }, []);
+  document.onmouseup = (e) => {
+    const textTemp = getSelectedText();
+
+    setText(textTemp);
+    if (buttonCss.left == "0") {
+      setButtonCss({
+        left: e.pageX + "px",
+        top: e.pageY + "px",
+      });
+    }
+  };
   const getSelectedText = () => {
     if (window.getSelection) {
       return window.getSelection().toString();
@@ -23,7 +64,19 @@ function Inject() {
     return "";
   };
 
-  return <div>{text}<br />- Loaded from text selection</div>;
+  return (
+    <div>
+      {text}
+      {textArea}
+      {text != "" && (
+        <button style={{ position: "absolute", ...buttonCss }}>Click</button>
+      )}
+      {textArea != "" && (
+        <button style={{ position: "absolute", ...buttonCss }}>Click</button>
+      )}
+      <br />- Loaded from text selection
+    </div>
+  );
 }
 window.addEventListener("load", () => {
   console.log("loaded");
@@ -31,9 +84,6 @@ window.addEventListener("load", () => {
   const injectDOM = document.createElement("div");
   injectDOM.className = "inject";
   injectDOM.style.textAlign = "center";
-  injectDOM.style.position = "absolute";
-  injectDOM.style.top = "100px";
-  injectDOM.style.left = "100px";
   document.body.appendChild(injectDOM);
   ReactDOM.render(<Inject />, injectDOM);
   // ReactDOM.render(<Inject />, document.getElementById('inject'));
